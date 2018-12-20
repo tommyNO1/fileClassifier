3013249a8b9551b18057dd44e9f995d2c830118b
Tommy_Service <905390519@qq.com>
2018-12-20 11:56:54 +0800

    MyClassifer类中添加了txt文件提取方法

--- src/com/company/MyClassifer.java
+++ src/com/company/MyClassifer.java
@@ -8,14 +8,30 @@ public class MyClassifer {
     public MyClassifer(File dir){
         this.dir = dir;
     }
+
     public File[] getFileDic(){
-        FileFilter fileFilter =new FileFilter() {
+        FileFilter dirFilter =new FileFilter() {
             @Override
             public boolean accept(File pathname) {
                 return pathname.isDirectory();
             }
         };
-        File[] files = this.dir.listFiles(fileFilter);
+        File[] files = this.dir.listFiles(dirFilter);
+        return files;
+    }
+
+    public File[] getFileTxt(){
+        FileFilter txtFileFilter =new FileFilter() {
+            @Override
+            public boolean accept(File file) {
+                String name = file.getName();
+                if(name.endsWith(".txt"))
+                    return true;
+                else
+                    return false;
+            }
+        };
+        File[] files = this.dir.listFiles(txtFileFilter);
         return files;
     }
 }
